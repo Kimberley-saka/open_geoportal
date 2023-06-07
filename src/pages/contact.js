b7e5ea27416cf0fedf8480094ff0e3267d9dbf3d
Kimberley saka <kimberleysaka@gmail.com>
2023-06-07 09:34:52 +0200

    added icons and a background

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,40 @@
+import './styling/contact-us.css';
+import twittersvg from './twittersvg.svg';
+import emailsvg from './emailsvg.svg';
+import phonesvg from './phonesvg.svg'
+import linkedinsvg from './linkedinsvg.svg';
+
 export default function Contact(){
 
-    return(
-        <div className="contact-us">
-            Countact Us
+  return(
+    <div className="contact-us">
+      <div className='contents'>
+        <p>
+          Get in touch
+        </p>
+        <div className='email-container'>
+          <img src={emailsvg} alt='email'
+        className='email-address'/>
         </div>
+
+        <div className='email-container'>
+          <img src={phonesvg} alt='email'
+        className='email-address'/>
+        </div>
+
+        <div className='email-container'>
+          <img src={linkedinsvg} alt='email'
+        className='email-address'/>
+        </div>
+
+        <div className='email-container'>
+          <img src={twittersvg}
+           alt='email'
+        className='email-address'/>
+        </div>
+
+      </div>
+        
+    </div>
     );
 }
